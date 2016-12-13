bedaf4335e8235c43e282dde790e15b9cf1fd90a
Simon Scholz <simon.scholz@vogella.com>
2016-12-13 13:21:48 +0100

    Added method for getting default propertyNames and propertyToLabelMap
    Change-Id: Ie1ddc0e93b35a8012891f5accef056125d46bbf3
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.model.service/src/com/vogella/model/service/PersonServiceImpl.java
+++ com.vogella.model.service/src/com/vogella/model/service/PersonServiceImpl.java
@@ -1,9 +1,12 @@
 package com.vogella.model.service;
+
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import org.osgi.service.component.annotations.Component;
@@ -15,105 +18,116 @@ import com.vogella.model.person.PersonService;
 import com.vogella.model.person.PersonWithAddress;
 
 /**
- * Class that acts as service for accessing numerous {@link Person}s.
- * The values are randomly put together out of names and places from "The Simpsons"
+ * Class that acts as service for accessing numerous {@link Person}s. The values
+ * are randomly put together out of names and places from "The Simpsons"
  * 
  * @author Dirk Fauth
  */
-@Component(service=PersonService.class)
+@Component(service = PersonService.class)
 public class PersonServiceImpl implements PersonService {
+
+	private static final String[] propertyNames = { "firstName", "lastName", "gender", "married", "birthday" };
+
+	private static final Map<String, String> propertyToLabelMap;
+
+	static {
+		propertyToLabelMap = new HashMap<>();
+		propertyToLabelMap.put("firstName", "Firstname");
+		propertyToLabelMap.put("lastName", "Lastname");
+		propertyToLabelMap.put("gender", "Gender");
+		propertyToLabelMap.put("married", "Married");
+		propertyToLabelMap.put("birthday", "Birthday");
+	}
 
 	@Override
 	public List<Person> getPersons(int numberOfPersons) {
 		List<Person> result = new ArrayList<Person>();
-		
+
 		for (int i = 0; i < numberOfPersons; i++) {
 			result.add(createPerson(i));
 		}
-		
+
 		return result;
 	}
 
 	@Override
 	public List<PersonWithAddress> getPersonsWithAddress(int numberOfPersons) {
 		List<PersonWithAddress> result = new ArrayList<PersonWithAddress>();
-		
+
 		for (int i = 0; i < numberOfPersons; i++) {
 			result.add(new PersonWithAddress(createPerson(i), createAddress()));
 		}
-		
+
 		return result;
 	}
-	
+
 	@Override
 	public List<ExtendedPersonWithAddress> getExtendedPersonsWithAddress(int numberOfPersons) {
 		List<ExtendedPersonWithAddress> result = new ArrayList<ExtendedPersonWithAddress>();
-		
+
 		for (int i = 0; i < numberOfPersons; i++) {
-			result.add(new ExtendedPersonWithAddress(createPerson(i), createAddress(), 
-					generateSimplePassword(), createRandomLengthText(), createRandomMoneyAmount(), 
-					createFavouriteFood(), createFavouriteDrinks()));
+			result.add(new ExtendedPersonWithAddress(createPerson(i), createAddress(), generateSimplePassword(),
+					createRandomLengthText(), createRandomMoneyAmount(), createFavouriteFood(),
+					createFavouriteDrinks()));
 		}
-		
+
 		return result;
 	}
-	
+
 	@Override
 	public Person createPerson(int id) {
 		Random randomGenerator = new Random();
-		
+
 		Person result = new Person(id);
 		result.setGender(Person.Gender.values()[randomGenerator.nextInt(2)]);
-		
+
 		if (result.getGender().equals(Person.Gender.MALE)) {
 			result.setFirstName(maleNames[randomGenerator.nextInt(maleNames.length)]);
-		}
-		else {
+		} else {
 			result.setFirstName(femaleNames[randomGenerator.nextInt(femaleNames.length)]);
 		}
-		
+
 		result.setLastName(lastNames[randomGenerator.nextInt(lastNames.length)]);
 		result.setMarried(randomGenerator.nextBoolean());
-		
+
 		int month = randomGenerator.nextInt(12);
 		int day = 0;
 		if (month == 2) {
 			day = randomGenerator.nextInt(28);
-		}
-		else {
+		} else {
 			day = randomGenerator.nextInt(30);
 		}
 		int year = 1920 + randomGenerator.nextInt(90);
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		try {
-			result.setBirthday(sdf.parse(""+year+"-"+month+"-"+day));
+			result.setBirthday(sdf.parse("" + year + "-" + month + "-" + day));
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
-	
+
 	@Override
 	public Address createAddress() {
 		String[] streets = getStreetNames();
-		int[] plz = {11111, 22222, 33333, 44444, 55555, 66666};
+		int[] plz = { 11111, 22222, 33333, 44444, 55555, 66666 };
 		String[] cities = getCityNames();
-		
+
 		Random randomGenerator = new Random();
-		
+
 		Address result = new Address();
-		
+
 		result.setStreet(streets[randomGenerator.nextInt(streets.length)]);
 		result.setHousenumber(randomGenerator.nextInt(200));
 		int cityRandom = randomGenerator.nextInt(cities.length);
 		result.setPostalCode(plz[cityRandom]);
 		result.setCity(cities[cityRandom]);
-		
+
 		return result;
 	}
-	
+
 	@Override
 	public String generateSimplePassword() {
 		String result = "";
@@ -124,13 +138,13 @@ public class PersonServiceImpl implements PersonService {
 		}
 		return result;
 	}
-	
+
 	@Override
 	public List<String> createFavouriteFood() {
 		String[] food = getFoodList();
 		Random rand = new Random();
 		int favCount = rand.nextInt(food.length);
-		
+
 		List<String> result = new ArrayList<String>();
 		for (int i = 0; i < favCount; i++) {
 			int randIndex = rand.nextInt(food.length);
@@ -140,13 +154,13 @@ public class PersonServiceImpl implements PersonService {
 		}
 		return result;
 	}
-	
+
 	@Override
 	public List<String> createFavouriteDrinks() {
 		String[] drinks = getDrinkList();
 		Random rand = new Random();
 		int favCount = rand.nextInt(drinks.length);
-		
+
 		List<String> result = new ArrayList<String>();
 		for (int i = 0; i < favCount; i++) {
 			int randIndex = rand.nextInt(drinks.length);
@@ -156,31 +170,31 @@ public class PersonServiceImpl implements PersonService {
 		}
 		return result;
 	}
-	
+
 	@Override
 	public String[] getStreetNames() {
 		return streetNames;
 	}
-	
+
 	@Override
 	public String[] getCityNames() {
 		return cityNames;
 	}
-	
+
 	@Override
 	public String[] getFoodList() {
 		return foodList;
 	}
-	
+
 	@Override
 	public String[] getDrinkList() {
 		return drinkList;
 	}
-	
+
 	@Override
 	public String createRandomLengthText() {
 		String[] words = baseText.split(" ");
-		
+
 		Random wordRandom = new Random();
 		String msg = "";
 		int randWords = wordRandom.nextInt(words.length);
@@ -192,15 +206,25 @@ public class PersonServiceImpl implements PersonService {
 				msg += " ";
 			}
 		}
-		
+
 		return msg;
 	}
-	
+
 	@Override
 	public Double createRandomMoneyAmount() {
 		Double result = new Random().nextDouble() * 1000;
 		BigDecimal bd = new BigDecimal(result);
 		bd = bd.setScale(2, BigDecimal.ROUND_HALF_UP);
 		return bd.doubleValue();
+	}
+
+	@Override
+	public String[] getDefaultPropertyNames() {
+		return propertyNames;
+	}
+
+	@Override
+	public Map<String, String> getDefaultPropertyToLabelMap() {
+		return propertyToLabelMap;
 	}
 }
--- com.vogella.model/src/com/vogella/model/person/PersonService.java
+++ com.vogella.model/src/com/vogella/model/person/PersonService.java
@@ -1,103 +1,72 @@
 package com.vogella.model.person;
 
 import java.util.List;
+import java.util.Map;
 
 public interface PersonService {
-	
-	public static String[] maleNames = {
-			"Bart", 
-			"Homer", 
-			"Lenny", 
-			"Carl", 
-			"Waylon", 
-			"Ned", 
-			"Timothy"};
-		public static String[] femaleNames = {
-			"Marge", 
-			"Lisa", 
-			"Maggie", 
-			"Edna", 
-			"Helen", 
-			"Jessica"};
-		public static String[] lastNames = {
-			"Simpson", 
-			"Leonard", 
-			"Carlson", 
-			"Smithers", 
-			"Flanders", 
-			"Krabappel", 
-			"Lovejoy"};
-		public static String[] streetNames = new String[] {
-			"Evergreen Terrace", 
-			"Main Street", 
-			"South Street", 
-			"Plympton Street", 
-			"Highland Avenue", 
-			"Elm Street", 
-			"Oak Grove Street"};
-		public static String[] cityNames = new String[] {
-			"Springfield", 
-			"Shelbyville", 
-			"Ogdenville", 
-			"Waverly Hills", 
-			"North Haverbrook", 
-			"Capital City"};
-		public static String[] foodList = new String[] {
-			"Donut",
-			"Bacon",
-			"Fish",
-			"Vegetables",
-			"Ham",
-			"Prezels"};
-		public static String[] drinkList = new String[] {
-			"Beer",
-			"Water",
-			"Soda",
-			"Milk",
-			"Coke",
-			"Fizzy Bubblech"};
-		public static String baseText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, " + 
-				"sed diam nonumy eirmod tempor invidunt ut labore et dolore " + 
-				"magna aliquyam erat, sed diam voluptua.";
+
+	public static String[] maleNames = { "Bart", "Homer", "Lenny", "Carl", "Waylon", "Ned", "Timothy" };
+	public static String[] femaleNames = { "Marge", "Lisa", "Maggie", "Edna", "Helen", "Jessica" };
+	public static String[] lastNames = { "Simpson", "Leonard", "Carlson", "Smithers", "Flanders", "Krabappel",
+			"Lovejoy" };
+	public static String[] streetNames = new String[] { "Evergreen Terrace", "Main Street", "South Street",
+			"Plympton Street", "Highland Avenue", "Elm Street", "Oak Grove Street" };
+	public static String[] cityNames = new String[] { "Springfield", "Shelbyville", "Ogdenville", "Waverly Hills",
+			"North Haverbrook", "Capital City" };
+	public static String[] foodList = new String[] { "Donut", "Bacon", "Fish", "Vegetables", "Ham", "Prezels" };
+	public static String[] drinkList = new String[] { "Beer", "Water", "Soda", "Milk", "Coke", "Fizzy Bubblech" };
+	public static String baseText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, "
+			+ "sed diam nonumy eirmod tempor invidunt ut labore et dolore " + "magna aliquyam erat, sed diam voluptua.";
 
 	/**
-	 * Creates a list of {@link Person}s. 
-	 * @param numberOfPersons The number of {@link Person}s that should be generated.
+	 * Creates a list of {@link Person}s.
+	 * 
+	 * @param numberOfPersons
+	 *            The number of {@link Person}s that should be generated.
 	 * @return
 	 */
 	List<Person> getPersons(int numberOfPersons);
 
 	/**
-	 * Creates a list of {@link PersonWithAddress}. 
-	 * @param numberOfPersons The number of {@link PersonWithAddress} that should be generated.
+	 * Creates a list of {@link PersonWithAddress}.
+	 * 
+	 * @param numberOfPersons
+	 *            The number of {@link PersonWithAddress} that should be
+	 *            generated.
 	 * @return
 	 */
 	List<PersonWithAddress> getPersonsWithAddress(int numberOfPersons);
 
 	/**
-	 * Creates a list of {@link ExtendedPersonWithAddress}. 
-	 * @param numberOfPersons The number of {@link ExtendedPersonWithAddress} that should be generated.
+	 * Creates a list of {@link ExtendedPersonWithAddress}.
+	 * 
+	 * @param numberOfPersons
+	 *            The number of {@link ExtendedPersonWithAddress} that should be
+	 *            generated.
 	 * @return
 	 */
 	List<ExtendedPersonWithAddress> getExtendedPersonsWithAddress(int numberOfPersons);
 
 	/**
-	 * Creates a random person out of names which are taken from "The Simpsons" 
+	 * Creates a random person out of names which are taken from "The Simpsons"
 	 * and enrich them with random generated married state and birthday date.
+	 * 
 	 * @return
 	 */
 	Person createPerson(int id);
 
 	/**
-	 * Creates a random address out of street names, postal codes and city names which are
-	 * taken from "The Simpsons" 
-	 * (i haven't found postal codes, so here i invented some for the example)
+	 * Creates a random address out of street names, postal codes and city names
+	 * which are taken from "The Simpsons" (i haven't found postal codes, so
+	 * here i invented some for the example)
+	 * 
 	 * @return
 	 */
 	Address createAddress();
 
 	/**
-	 * @return A simple password consisting of 8 characters in the value ranges a-z, A-Z
+	 * @return A simple password consisting of 8 characters in the value ranges
+	 *         a-z, A-Z
 	 */
 	String generateSimplePassword();
 
@@ -112,12 +81,14 @@ public interface PersonService {
 	List<String> createFavouriteDrinks();
 
 	/**
-	 * @return An array of street names that are also used to create random addresses.
+	 * @return An array of street names that are also used to create random
+	 *         addresses.
 	 */
 	String[] getStreetNames();
 
 	/**
-	 * @return An array of city names that are also used to create random addresses.
+	 * @return An array of city names that are also used to create random
+	 *         addresses.
 	 */
 	String[] getCityNames();
 
@@ -140,5 +111,9 @@ public interface PersonService {
 	 * @return A random money amount between 0 and 1000.
 	 */
 	Double createRandomMoneyAmount();
+
+	String[] getDefaultPropertyNames();
+
+	Map<String, String> getDefaultPropertyToLabelMap();
 
 }
