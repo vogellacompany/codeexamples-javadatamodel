5e056f17ebfe6cc8abd08d3a551acb666b9e7e6d
Simon Scholz <simon.scholz@vogella.com>
2016-12-14 23:23:53 +0100

    Added PropertyChangeSupport to the Person class
    Change-Id: Iee4d55cd926579cf13e2b5fedbc63d4bfe1c74db
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.model/src/com/vogella/model/person/Person.java
+++ com.vogella.model/src/com/vogella/model/person/Person.java
@@ -1,23 +1,27 @@
 package com.vogella.model.person;
+
+import java.beans.PropertyChangeSupport;
 import java.util.Date;
 
 public class Person {
 
+	protected final PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+
 	public enum Gender {
 		MALE, FEMALE
 	}
-	
+
 	private final int id;
 	private String firstName;
 	private String lastName;
 	private Gender gender;
 	private boolean married;
 	private Date birthday;
-	
-	public Person(int id) { 
+
+	public Person(int id) {
 		this.id = id;
 	}
-	
+
 	public Person(int id, String firstName, String lastName, Gender gender, boolean married, Date birthday) {
 		this.id = id;
 		this.firstName = firstName;
@@ -26,53 +30,53 @@ public class Person {
 		this.married = married;
 		this.birthday = birthday;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
-	
+
 	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+		changeSupport.firePropertyChange("firstName", this.firstName, this.firstName = firstName);
 	}
-	
+
 	public String getLastName() {
 		return lastName;
 	}
-	
+
 	public void setLastName(String lastName) {
-		this.lastName = lastName;
+		changeSupport.firePropertyChange("lastName", this.firstName, this.lastName = lastName);
 	}
-	
+
 	public Gender getGender() {
 		return gender;
 	}
-	
+
 	public void setGender(Gender gender) {
-		this.gender = gender;
+		changeSupport.firePropertyChange("gender", this.gender, this.gender = gender);
 	}
-	
+
 	public boolean isMarried() {
 		return married;
 	}
-	
+
 	public void setMarried(boolean married) {
-		this.married = married;
+		changeSupport.firePropertyChange("married", this.married, this.married = married);
 	}
-	
+
 	public Date getBirthday() {
 		return birthday;
 	}
-	
+
 	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
+		changeSupport.firePropertyChange("birthday", this.birthday, this.birthday = birthday);
 	}
-	
+
 	@Override
 	public String toString() {
-		return getFirstName() + " " + getLastName() + " " + getBirthday();
+		return getFirstName() + " " + getLastName();
 	}
 }
